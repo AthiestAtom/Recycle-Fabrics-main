082b6bd4d3a3e04c0aaef956bf572897ded56e38
Jashan Bansal <jashanbansal1101@gmail.com>
2026-03-07 23:01:49 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }: { mode: string }) => ({
   server: {
     host: "0.0.0.0",
     port: 8080,
